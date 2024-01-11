7300d4f198b7f1b615f8b1c4532c0001047784c6
Oluwadamilare Idowu Joshua <oluwadamilareidowujoshua@gmail.com>
2024-01-11 13:20:24 +0100

    first commit

---
+++ Simple Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple Calculator
+//
+//  Created by Idowu Oluwadamilare on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
